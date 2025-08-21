7af64bf2a5607f988b09ed12885017c518e4345b
gfianu <delyouandme@gmail.com>
2025-08-20 21:47:01 -0400

    Completed Clothing Project with Robyn & Mary Ann

--- pairWork.js
+++ pairWork.js
@@ -1,3 +1,17 @@
+/*
+Instructions
+Clothing
+Define a Clothing class with properties name, color, material, and isDirty.
+isDirty should always be false at instantiation of an object.
+Add a describe() method that prints a statement using the values stored in the object's other properties. The statement should read differently depending on whether the clothing item is dirty or not.
+Add a wear() method outside the constructor that changes the value of isDirty and prints a notification.
+Add a launder() method outside the constructor that changes the value of isDirty and prints a notification.
+Instantiate at least 3 different kinds of clothing with unique properties.
+Use the describe() method to print descriptions of each clothing item.
+Use the wear() method on two of the clothing items.
+Use the launder() method on one of the dirty clothing items and describe() it again.
+*/
+// Clothing Class
 class Clothing{
     constructor(name, color, material){
         this.name = name;
@@ -5,7 +19,7 @@ class Clothing{
         this.material = material;
         this.isDirty = false;
     }
-
+    // creating methods in Clothing class
     describe() {
         if (this.isDirty){
             console.log(`This ${this.color} ${this.material} ${this.name} is dirty!!! WASH NOW!`);
@@ -24,11 +38,16 @@ class Clothing{
     }
 }
 
+// Instantiating three different kinds of clothing clothing 
 let tShirt = new Clothing ("T-Shirt", "Red", "Cotton");
 let jeans = new Clothing ("Apple-Bottomed Jeans", "Black", "Denim");
 let boots = new Clothing ("Boots with the fur", "Grey", "Leather");
 let jacket = new Clothing ("Fall Jacket", "Tan", "Wool");
 
+/* Using the describe() method to print descriptions of each clothing item.
+Using the wear() method on two of the clothing items.
+Using the launder() method on one of the dirty clothing items and describe() it again.
+*/
 tShirt.describe();
 jeans.describe();
 boots.describe();
